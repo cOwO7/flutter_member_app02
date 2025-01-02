362c7cd6ca61066a9f32b6ab53d6e0ebca3c6ac9
cOwO7 <oclc00@naver.com>
2025-01-02 10:21:13 +0900

    회원가입/조회 앱 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -38,18 +38,50 @@ class _MainActivityState extends State<MainActivity> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('cOwO7 :',
-            style: Theme.of(context).textTheme.headlineLarge ),
+      body: Padding(padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 150,),
+              Text('회원가입 앱', style: TextStyle(fontSize: 34), textAlign: TextAlign.center,),
+              TextField(decoration: const InputDecoration(hintText: '아이디 입력'),),
+              TextField(decoration: const InputDecoration(hintText: '비밀번호 입력'),),
+              TextField(decoration: const InputDecoration(hintText: '이름 입력'),),
+              TextField(decoration: const InputDecoration(hintText: '이메일 입력'),),
+              SizedBox(height: 16,),
+              ElevatedButton(onPressed: _registerUser, child: Container(
+                width: double.infinity, child: Text('회원 가입', textAlign: TextAlign.center,),
+              )),
+              SizedBox(height: 16,),
+              ElevatedButton(onPressed: _listUsers, child: Container(
+                width: double.infinity, child: Text('회원 조회', textAlign: TextAlign.center,),
+              ))
           ],
         ),
-      ),
+      ), // SingleChildScrollView
+    ),
     );
-  }
-}
+  } // build
+
+ // 회원가입 처리
+  Future<void> _registerUser() async {
+    // showSnackBar : 간단한 메세지 출력
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('회원가입 기능 구현중...'))
+    );
+  } // _registerUser
+  // 회원조회 처리
+  Future<void> _listUsers() async {
+    // showSnackBar : 간단한 메세지 출력
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('회원조회 기능 구현중...'))
+    );
+  } // _listUsers
+} // _MainActivityState
+
