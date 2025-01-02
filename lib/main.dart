5c449e826b1dc29a1d559bc6a980a5e0a5ea7f4e
cOwO7 <oclc00@naver.com>
2025-01-02 09:52:13 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '플러터 회원가입 앱',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainActivity(title: ' cOwO7 '),
+    );
+  }
+}
+
+class MainActivity extends StatefulWidget {
+  const MainActivity({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MainActivity> createState() => _MainActivityState();
+}
+
+class _MainActivityState extends State<MainActivity> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('cOwO7 :',
+            style: Theme.of(context).textTheme.headlineLarge ),
+          ],
+        ),
+      ),
+    );
+  }
+}
